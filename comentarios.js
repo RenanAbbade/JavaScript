931343813288b2c8696305bc5bcfbe0f07d74e9e
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2018-08-29 16:33:59 -0300

    Create comentarios.js

---
+++ comentarios.js
@@ -0,0 +1,12 @@
+// Comentário de linha
+console.log('Linha1')
+
+/* Comentário de múltiplas rotinas */
+console.log('Linha 2')
+
+/*
+*Comentário de varias linhas
+*Código claro e limpo
+*Evitar comentários obvios que 
+*tem o mesmo nome que funções
+*/
