a2562b89871b706bdd41689a9420b1f55a33385e
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2018-08-29 17:20:07 -0300

    Create Var&Const.js

---
+++ Var&Const.js
@@ -0,0 +1,12 @@
+/* Os dados são organizados em estruturas de dados(array,listas,blocos)
+Variaveis e constantes em ES JS
+*/
+var a = 3 //Atribuição(=) 
+let b = 4 /*let cria variaveis também em JS, é o mais apropriado.*/
+
+
+b = 40 /*Posso atribuir outro valor a memsma variável */
+console.log(a,b)
+
+const c = 5/*Por ser uma constante não posso atribuir outro valor */
+console.log(c)
